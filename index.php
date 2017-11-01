19dc6df7c6f35b4936fa91c8b0257466a7a6f3b4
Snyder <os4584kh@os4584khm7.workstations.winona.edu>
2017-11-01 15:21:22 -0500

    added bootstrap cdn

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="option2.css">
 	<title>Green Fee</title>
 </head>
 <body>
+
+	
+
+	
 
 </body>
 </html>
