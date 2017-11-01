4081ef3495296b80d11431003318a6236374bb5c
Snyder <os4584kh@os4584khm7.workstations.winona.edu>
2017-11-01 15:02:56 -0500

    index Page
    index

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="option2.css">
+	<title>Green Fee</title>
+</head>
+<body>
 
+	
+
+
+</body>
+</html>
