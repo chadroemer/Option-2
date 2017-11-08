3b80f8246b70423a2881c92b79b1ae7fe45edd7b
Snyder <os4584kh@os4584khm7.workstations.winona.edu>
2017-11-08 16:19:39 -0600

    added greenfee icon to tab icon

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css">
-	<link rel="icon" href="photos/green.png">
+	<link rel="icon" href="photos/WSU_GreenFee.png">
 	<link rel="stylesheet" type="text/css" href="option2.css">
 	<title>Application Form</title>
 
