6bcb827d1e1a9a783bb4b777590110df31ac1838
Snyder <os4584kh@os4584khm7.workstations.winona.edu>
2017-11-08 20:23:24 -0600

    Took out irrelevent code

--- form.php
+++ form.php
@@ -29,16 +29,6 @@
 
 	<div id="greenFeeIntro">
 
-<<<<<<< HEAD
-
-
-
-
-
-		
-
-=======
->>>>>>> origin/master
 		<div id="tabs">
 		  <ul>
 		    <li><a href="#tabs-1">Form Tab 1</a></li>
@@ -48,18 +38,15 @@
 		  </ul>
 		  <div id="tabs-1">
 			
-<<<<<<< HEAD
 			<form id="FormTab1" action="confirmation.php" method="post">
 				<p>Project Title</p>
 				<hr id="formHr">
 				<input type="text" name="projectTitle" id="inputSize" required>
 
-=======
 			<form id="FormTab1" method="post">
 				<p>Project Title</p>
 				<hr id="formHr">
 				<input type="text" name="projectTitle" id="inputSize" required>
->>>>>>> origin/master
 
 				<p>Total Amount Requested</p>
 				<hr id="formHr">
