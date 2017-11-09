658b31ae80f5018b93909dba295a4ff008597f69
emhewitt1991 <emhewitt1991@users.noreply.github.com>
2017-11-08 20:26:48 -0600

    cleaned up test.php

--- test.php
+++ test.php
@@ -4,36 +4,33 @@
  */
 if(isset($_POST['username']) && isset($_POST['password'])){
 
-    $adServer = "ldap://dc1.winona.edu";
+    $dc = "ldap://dc1.winona.edu";
 	
-    $ldap = ldap_connect($adServer);
+    $dc_connect = ldap_connect($dc);
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $ldaprdn = 'winona' . "\\" . $username;
+    $dc_connect_user = 'winona' . "\\" . $username;
 
-    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
-    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+    ldap_set_option($dc_connect, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($dc_connect, LDAP_OPT_REFERRALS, 0);
 
-    $bind = @ldap_bind($ldap, $ldaprdn, $password);
+    $bind = @ldap_bind($dc_connect, $dc_connect_user, $password);
 
 
     if ($bind) {
         $filter="(sAMAccountName=$username)";
-        $result = ldap_search($ldap,"dc=winona,dc=edu",$filter);
-        ldap_sort($ldap,$result,"sn");
-        $info = ldap_get_entries($ldap, $result);
+        $result = ldap_search($dc_connect,"dc=winona,dc=edu",$filter);
+        ldap_sort($dc_connect,$result,"sn");
+        $info = ldap_get_entries($dc_connect, $result);
         for ($i=0; $i<$info["count"]; $i++)
         {
             if($info['count'] > 1)
                 break;
             echo "<p>Welcome <strong> ". $info[$i]["sn"][0] .", " . $info[$i]["givenname"][0] ."</strong><br /> Your username is:  <strong>(" . $info[$i]["samaccountname"][0] .")</strong></p>\n";
-           // echo '<pre>';
-           // var_dump($info);
-           // echo '</pre>';
-           // $userDn = $info[$i]["distinguishedname"][0]; 
+
         }
-        @ldap_close($ldap);
+        @ldap_close($dc_connect);
     } else {
         $msg = "Invalid username / password";
         echo $msg;
