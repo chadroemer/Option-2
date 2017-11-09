b590140d524de6cc2e20fb33cdcea8ca83eb142d
Snyder <os4584kh@os4584khm7.workstations.winona.edu>
2017-11-08 21:49:25 -0600

    wrecked index page

--- index.php
+++ index.php
@@ -8,12 +8,84 @@
 	<link rel="stylesheet" type="text/css" href="option2.css">
 	<title>Green Fee Application</title>
 </head>
-<body>
+<body class="index">
 	
 	<div id="header">
 		<img id="mainInfoPic" src="photos/WinonaWhite.png">		
 	</div>
-	<div id="greenFeeIntro">
+	<div>
+		
+		<!-- <img id="indexBack" src="photos/SugarLoafc.jpg"> -->
+
+	<div class="indexMain" id="leftIndex">
+
+		<div id="greenFeeIntro">
+		<br>
+		<h2 id="greenFeeh2">WSU Student Green Fee Grant Application <i class="fa fa-leaf" aria-hidden="true"></i></h2>
+
+			<p>The <span>WSU Student Green Fee (SGF)</span> provides funding for projects that promote environmental sustainability and enhance the student experience at WSU.  SGF funding will be allocated to projects that increase the use of renewable energy on campus and/or in the local community, increase the energy efficiency of our facilities, reduce the amount of waste created and material resources used on campus, encourage sustainable behaviors, and integrate sustainability into teaching, research, operations, buildings and grounds management at WSU. The All-University Sustainability Committee administers the SGF.
+			</p>
+
+			<p><strong><u>Application Instructions:</u></strong></p>
+
+			<p>Please review the <a href="http://cs.winona.edu/Francioni/WSU-green_files/SGF-project-guidelines.pdf" target="_blank">Student Green Fee Project Application Guidelines</a> before submitting your application. It is also recommended that you contact the Sustainability Director in advance of the application deadline to talk through your application before you submit it. Contact:
+			</p>
+
+			<p id="contactInfo">
+				Nathan Engstrom<br>
+				Sustainability Director<br>
+				(507) 457-2395 <br>
+				<a href="mailto:nengstrom@winona.edu?subject=SGF%20application">NEngstrom@winona.edu</a>
+			</p>
+
+			<p>When you are ready to fill out your application, please answer the questions in this form. You should submit additional documentation (e.g., letters of support, maps, drawings, budget, etc.) via email to <a href="mailto:nengstrom@winona.edu?subject=SGF%20application">NEngstrom@winona.edu.</a>
+			</p>
+
+			<p><strong>You may save and continue working on the application at a later date, for up to ONE MONTH.</strong></p>
+
+			<hr id="introHr">
+			<br>
+			<br>
+				<div class="Button">
+					<a href="form.php">
+						<!-- <button>Next <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+						</button> -->
+					</a>
+				</div>
+	</div>
+		
+	</div>
+	<div class="indexMain" id="rightIndex">
+		
+		<div id="loginForm">
+			<p>Green Fee Portal Login</p>
+			<hr>
+			<form> 
+				<p>Please type in username: <i class="fa fa-user" aria-hidden="true"></i></p>
+				<input type="text" name="" id="inputSize" placeholder="Username">
+				<p>Please type in password: <i class="fa fa-unlock-alt" aria-hidden="true"></i></p>
+				<input type="text" name="" id="inputSize" placeholder="Password">
+				<br><br>
+				<button>Login <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+				</button>
+			</form>
+		</div>
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <p id="credit">Survey made by CS344 class 11/1/2017</p> -->
+	</div>
+	<!-- <div id="greenFeeIntro">
 		<br>
 		<h2 id="greenFeeh2">WSU Student Green Fee Grant Application <i class="fa fa-leaf" aria-hidden="true"></i></h2>
 
@@ -46,8 +118,8 @@
 						</button>
 					</a>
 				</div>
-	</div>
-	<p id="credit">Survey made by CS344 class 11/1/2017</p>
+	</div> -->
+	<!-- p id="credit">Survey made by CS344 class 11/1/2017</p> -->
 
 	<!-- ʕ•́ᴥ•̀ʔ -->
 
