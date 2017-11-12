809967690c9632722e5a11781bac35de12db7095
Eric Hewitt <emhewitt1991@gmail.com>
2017-11-11 22:56:35 -0600

    Added the AD stuff into a php chunk at the top of index.php.  Still need to figure out how to re-direct to next page after the AD lookup

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+	if(isset($_POST['username']) && isset($_POST['password']))
+	{
+		$dc = "ldap://dc1.winona.edu";
+	
+		$dc_connect = ldap_connect($dc);
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		$dc_connect_user = 'winona' . "\\" . $username;
+
+		ldap_set_option($dc_connect, LDAP_OPT_PROTOCOL_VERSION, 3);
+		ldap_set_option($dc_connect, LDAP_OPT_REFERRALS, 0);
+
+		$bind = @ldap_bind($dc_connect, $dc_connect_user, $password);
+
+
+		if ($bind) 
+		{
+			$filter="(sAMAccountName=$username)";
+			$result = ldap_search($dc_connect,"dc=winona,dc=edu",$filter);
+			ldap_sort($dc_connect,$result,"sn");
+			$info = ldap_get_entries($dc_connect, $result);
+			for ($i=0; $i<$info["count"]; $i++)
+			{
+				if($info['count'] > 1)
+					break;
+				echo "<p>Welcome <strong> ". $info[$i]["sn"][0] .", " . $info[$i]["givenname"][0] ."</strong><br /> Your username is:  <strong>(" . $info[$i]["samaccountname"][0] .")</strong></p>\n";
+
+			}
+			@ldap_close($dc_connect);
+		} 
+		else 
+		{
+			$msg = "Invalid username / password";
+			echo "<script type='text/javascript'>alert('$msg');</script>";
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -54,13 +94,13 @@
 		<div id="loginForm">
 			<p>Green Fee Portal Login</p>
 			<hr>
-			<form> 
+			<form method="POST"> 
 				<p><i class="fa fa-user" aria-hidden="true"></i> Please type in StarID/Username: </p>
-				<input type="text" name="" id="inputSize" placeholder="Username">
+				<input type="text" name="username" id="inputSize" placeholder="Username">
 				<p><i class="fa fa-unlock-alt" aria-hidden="true"></i> Please type in Password: </p>
-				<input type="Password" name="" id="inputSize" placeholder="Password">
+				<input type="Password" name="password" id="inputSize" placeholder="Password">
 				<br><br>
-				<button type="button">Login <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></button>
+				<button type="submit" formaction="#">Login <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></button>
 			</form>
 		</div>
 	</div>
