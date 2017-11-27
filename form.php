cde5cf8bf48c6fc9779857985944128d3bfb3f50
chadroemer <croemer13@winona.edu>
2017-11-27 12:43:46 -0600

    made the 'Group' tab disappear when they select 'Staff"

--- form.php
+++ form.php
@@ -8,6 +8,7 @@
 	<link rel="icon" href="photos/greenFee.png">
 	<link rel="stylesheet" type="text/css" href="option2.css">
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+	<script type="text/javascript" src="option2.js" > </script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <title>Application Form</title>
@@ -68,7 +69,7 @@
 				<p>Status:</p>
 					<hr id="formHr">
 					  	<input type="radio" name="status" value="student">Student<br>
-	  					<input type="radio" name="status" value="staff">Staff<br>
+	  					<input type="radio" name="status" value="staff" onclick="removeDummy();" >Staff<br>
 	  					<input type="radio" name="status" value="other">Other
 
 					<br><br>
--- option2.js
+++ option2.js
@@ -1,1 +1,6 @@
-
+function removeDummy() {
+ var elem = document.getElementById('tabs-2');
+ var elem2 = document.getElementById('tabs-3');
+ elem.parentNode.removeChild(elem);
+  elem2.parentNode.removeChild(elem);
+}
